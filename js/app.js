aacbba60950445385309106d2bd685a504e8aa04
Deceit-87 <manuelfreund318@gmail.com>
2022-02-17 23:19:50 +0100

    aggiunto new member con problemi

--- js/app.js
+++ js/app.js
@@ -32,30 +32,96 @@ const team = [
   ];
 
 
-  console.log(team[0].name,team[0].role,team[0].image)
-  console.log(team.length)
+//   console.log(team[0].name,team[0].role,team[0].image)
+//   console.log(team.length)
   const CONTAINER = document.querySelector('.team-container')
-  
+  CONTAINER.style.justifyContent = 'start'
+  CONTAINER.style.margin = '0 auto'
   for (let i = 0; i < team.length; i++) {
 
       console.log(team[i].name,team[i].role)
-      CONTAINER.innerHTML += `'  <div class="team-card">
+      CONTAINER.innerHTML += ` <div class="team-card">
       <div class="card-image">
         <img
           src="./img/${team[i].image}"
-          alt="Wayne Barnett"
-        >
+          alt=${team[i].name}
       </div>
       <div class="card-text">
         <h3>${team[i].name}</h3>
         <p>${team[i].role}</p>
       </div>
-    </div>'`
+    </div>`
+
+    const teamCard = document.querySelectorAll(".team-card")
+   
+    teamCard[i].style.marginRight = '0'
+    teamCard[i].style.flexBasis = 'calc(100% / 3 )'
+    teamCard[i].style.padding = '10px'
+    console.log(teamCard[i]) 
       
   }
 
+
+//   const userName = document.getElementById('name').value
+//   const userRole = document.getElementById('role').value
+//   const userImg = document.getElementById('image').value
+  const Add = document.getElementById('addMemberButton')
+
   
-  console.log(CONTAINER)
+
+//   console.log(userName,userRole,userImg)
+
+
+const userName = document.getElementById('name').value
+const userRole = document.getElementById('role').value
+const userImg = document.getElementById('image').value
+
+
+
+  function newMember(uName,uRole,uImg){
+
+    return {
+
+        name: uName,
+        role: uRole,
+        img: uImg
+ }
+
+  }
+
+  const AddMember = newMember(userName,userRole,userImg)
+  console.log(AddMember)
+
+  
+
+  function aggiungiMembro(AddMember){
+   
+    // team.push(AddMember)
+    
+     const memberCard = `
+    <div class="team-card">
+      <div class="card-image">
+        <img
+          src="img/${userImg}"
+          alt="${userName}"
+        />
+      </div>
+      <div class="card-text">
+        <h3>${userName}</h3>
+        <p>${userRole}</p>
+      </div>
+  </div>
+  `
+ CONTAINER.innerHTML += memberCard
+}
+
+
+
+Add.addEventListener('click',aggiungiMembro())
+
+// console.log(Add.addEventListener('click',))
+
+
 
   
 
